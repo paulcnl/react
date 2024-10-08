aa6eb7d0d2b84bc4a47e14aa30e49c74cfb088bd
paulcnl <paul.canarelli.2504@gmail.com>
2024-10-08 16:50:19 +0200

    PokemonCard

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+
 import "./index.css";
 
-const root = document.getElementById("root");
-
-if (root != null) {
-  createRoot(root).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-}
+createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
