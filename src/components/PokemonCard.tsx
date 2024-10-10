e07281fd7ee714f38eeff5da1498ab4c46843ae0
paulcnl <paul.canarelli.2504@gmail.com>
2024-10-10 15:39:46 +0200

    pokedex-3

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -10,25 +10,18 @@ const pokemonList = [
 ];
 
 function PokemonCard() {
-  const pokemon = {
-    name: pokemonList[0].name,
-    img: pokemonList[0].imgSrc,
-  };
+  const pokemon = pokemonList[1];
 
-  if (
-    (pokemon.imgSrc =
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png")
-  ) {
-    return (
-      <figure>
-        <figcaption>{pokemon.name}</figcaption>
-
-        <img src={pokemon.img} alt={pokemon.name} />
-      </figure>
-    );
-  } else {
-    return <p>????????</p>;
-  }
+  return (
+    <figure>
+      {pokemon.imgSrc != null ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???????</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
 }
 
 export default PokemonCard;
